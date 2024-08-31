c63d7dee5bd5566a9950550dd92bc4852fd878c9
hyeokkyu <hyeokkyu@insignary.com>
2024-08-31 14:40:44 +0900

    init

---
+++ src/main/java/springstudy/daangnclone/DaangncloneApplication.java
@@ -0,0 +1,13 @@
+package springstudy.daangnclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaangncloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaangncloneApplication.class, args);
+	}
+
+}
